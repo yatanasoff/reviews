4729f27cfb9615448b7808e267a71c7e6a131307
yordan <yordan@localhost.localdomain>
2025-07-11 16:38:46 +0300

    init

---
+++ main.py
@@ -0,0 +1,67 @@
+"""
+Test
+
+curl -X POST -H "Content-Type: application/json" -d '{"text":"хороший запах"}' http://127.0.0.1:8000/reviews
+curl -X POST -H "Content-Type: application/json" -d '{"text":"мне нравится"}' http://127.0.0.1:8000/reviews
+curl -X POST -H "Content-Type: application/json" -d '{"text":"ненавижу запах"}' http://127.0.0.1:8000/reviews
+curl  http://127.0.0.1:8000/reviews
+
+"""
+
+import datetime
+from contextlib import asynccontextmanager
+from typing import Optional
+
+from fastapi import FastAPI, status
+from peewee import *
+from pydantic import BaseModel
+
+db = SqliteDatabase('reviews.db', check_same_thread=False)
+
+
+class ReviewPydantic(BaseModel):
+    text: str
+    sentiment: Optional[str] = None
+    created_at: Optional[datetime.datetime] = None
+
+
+class Review(Model):
+    text: str = TextField()
+    sentiment: str = TextField()
+    created_at: datetime.datetime = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = db
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    db.connect()
+    db.create_tables([Review])
+    yield
+    db.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+def check_includes(keyworkds: list, text: str) -> bool:
+    return any(map(lambda k: k in text, keyworkds))
+
+
+@app.post("/reviews", status_code=status.HTTP_204_NO_CONTENT)
+async def create_reviews(review: ReviewPydantic):
+    if check_includes(["хорош", "люблю"], review.text):
+        sentiment = "positive"
+    elif check_includes(["плохо", "ненавиж"], review.text):
+        sentiment = "negative"
+    else:
+        sentiment = "neutral"
+    r = Review(text=review.text, sentiment=sentiment, created_at=datetime.datetime.now())
+    r.save()
+    return True
+
+
+@app.get("/reviews")
+async def read_reviews():
+    return [{"id": r.id, "text": r.text, "sentiment": r.sentiment, "created_at": r.created_at} for r in Review.select()]
